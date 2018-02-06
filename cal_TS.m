0fc91e54f078d3ce3aeb1dbbaa459d26cd648884
jimmy <973842075@qq.com>
2018-02-06 19:06:26 +0800

    The day before winter vocation. Eigen values are not finished

--- cal_TS.m
+++ cal_TS.m
@@ -1,9 +1,12 @@
 clear all;
-syms ul zl rl gl el pl sl us
-syms ur zr rr gr er pr sr
+%syms ul zl rl gl el pl sl us
+%syms ur zr rr gr er pr sr
+%eqn = ((ul - zl/(rl*gl))*us - 1)*((er*zr+pr*ur)*us - sr*zr - pr) ...
+%- ((ur - zr/(rr*gr))*us - 1)*((el*zl+pl*ul)*us - sl*zl - pl);
+%eqn = simplify(expand(eqn))
+%collect(eqn, us)
 
-eqn = ((ul - zl/(rl*gl))*us - 1)*((er*zr+pr*ur)*us - sr*zr - pr) ...
-- ((ur - zr/(rr*gr))*us - 1)*((el*zl+pl*ul)*us - sl*zl - pl);
-eqn = simplify(expand(eqn))
-collect(eqn, us)
+syms r u p g G
+A = [-1/g/r^2,   -u*g/r.   0;
+     -
 
